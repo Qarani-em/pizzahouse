30744f8654ac095a9c3341e810412c0f9660511e
Qarani-m <emqarani@gmail.com>
2023-04-16 20:01:23 +0300

    added routes

--- base/urls.py
+++ base/urls.py
@@ -2,5 +2,10 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.home, name ="home")
+    path("", views.home, name ="home"),
+    path("menu", views.menu, name ="menu"),
+    path("services", views.services, name ="services"),
+    path("blog", views.blog, name ="blog"),
+    path("about", views.about, name ="about"),
+    path("contact", views.contact, name ="contact"),
 ]
--- base/views.py
+++ base/views.py
@@ -4,3 +4,23 @@ from django.shortcuts import render
 
 def home(request):
     return render(request, "index.html")
+
+def menu(request):
+    return render(request, "menu.html")
+
+def services(request):
+    return render(request, "services.html")
+def blog(request):
+    return render(request, "blog.html")
+def about(request):
+    return render(request, "about.html")
+def contact(request):
+    return render(request, "contact.html")
+
+
+#   path("", views.home, name ="home"),
+#     path("menu", views.menu, name ="menu")
+#     path("services", views.services, name ="services")
+#     path("blog", views.blog, name ="blog")
+#     path("about", views.about, name ="about")
+#     path("contact", views.contact, name ="contact")'
